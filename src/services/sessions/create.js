178797ff6c72be1393ede0ecdc5e1c176480ab79
Yevhenii Krotenko <kroegen4@gmail.com>
2026-09-10 12:26:07 +0200

    Upgrade jsonwebtoken from v8.5.1 to v9.0.3
    - Change expiresIn from string '1h' to milliseconds (TOKEN_EXPIRY_MS = 3600000)
    - Convert to ES modules with explicit JSON module import
    - All 12 tests pass

--- src/services/sessions/create.js
+++ src/services/sessions/create.js
@@ -1,16 +1,18 @@
-const jwt     = require('jsonwebtoken');
-const User    = require('../../models/User');
-const utils   = require('../../utils');
-const { app } = require('../../bin/config.json');
-const { STATUS_SUCCESS, STATUS_FAILURE } = utils.statusCodes;
+import jwt from 'jsonwebtoken';
+import User from '../../models/User.js';
+import utils from '../../utils/index.js';
+import config from '../../bin/config.json' with { type: 'json' };
+import { STATUS_SUCCESS, STATUS_FAILURE } from '../../utils/statusCodes.js';
 
-exports.createSession = async(req, res, next) => {
+const TOKEN_EXPIRY_MS = 60 * 60 * 1000; // 1 hour in milliseconds
+
+export const createSession = async(req, res, next) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email });
 
         if (user && user.checkPassword(password)) {
-            const token = jwt.sign(utils.dump.dumpUser(user), app.secret, { expiresIn: '1h' });
+            const token = jwt.sign(utils.dump.dumpUser(user), config.app.secret, { expiresIn: TOKEN_EXPIRY_MS });
 
             await res.send({ status: STATUS_SUCCESS, data: { token } });
         } else {

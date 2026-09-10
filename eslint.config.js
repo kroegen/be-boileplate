2a1c2a02b053140f69d77f93e2bf1aa10ad3d5a5
Yevhenii Krotenko <kroegen4@gmail.com>
2026-09-10 21:31:37 +0200

    remove commonjs eslint settings

--- eslint.config.js
+++ eslint.config.js
@@ -12,24 +12,6 @@ export default [
       'src/services/sessions/create.js',
     ],
   },
-  // CommonJS source files
-  {
-    files: ['src/**/*.js', '!src/services/**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2024,
-      sourceType: 'commonjs',
-      globals: globals.node,
-    },
-    rules: {
-      'no-console': 'warn',
-      'no-debugger': 'warn',
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      eqeqeq: ['error', 'always'],
-      curly: ['error', 'all'],
-      'prefer-const': 'off',
-      'no-var': 'error',
-    },
-  },
   // ESM files (services, tests, config)
   {
     files: ['src/services/**/*.js', 'tests/**/*.js', '*.js', 'vitest.config.js'],
